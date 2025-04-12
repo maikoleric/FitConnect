ceaf38c04ae0660ade887a2d708f8954e3fbea73
maikoleric <kolericmai@gmail.com>
2025-04-12 11:36:04 +0000

    novo spremenjeno z tinydb

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-from flask import Flask, render_template, request, redirect, session, url_for
-from models import db, User
-import os
+from flask import Flask, render_template, request, redirect, session
+from tinydb import TinyDB, Query
+import requests
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SECRET_KEY'] = 'ja'
+app.config['SECRET_KEY'] = 'skrivnost'
 
-db.init_app(app)
-
-# ===================== LANDING PAGE =====================
-@app.route('/landing')
-def landing():
-    return render_template('index.html')
+db = TinyDB('users.json')
+User = Query()
 
 @app.route('/')
-def root():
-    return redirect('/landing')
+def landing():
+    quote_data = requests.get('https://zenquotes.io/api/random').json()
+    quote = {
+    'text': quote_data[0].get('q') or 'Vztrajaj, tudi ko je težko.',
+    'author': quote_data[0].get('a') or 'Neznan'
+}
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    location_data = requests.get('https://ipapi.co/json/').json()
+    city = location_data.get('city', 'neznano')
+    country = location_data.get('country_name', 'neznano')
+
+    return render_template('index.html', quote=quote, city=city, country=country)
